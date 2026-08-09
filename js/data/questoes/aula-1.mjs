68737f693ab36084b73aaa7c44fdfffb28c41954
elanunesm <elaine-nunes-m@live.com>
2026-08-09 20:40:41 -0300

    Atualização do projeto — 09/08/2026, 20:40:41

--- js/data/questoes/aula-1.mjs
+++ js/data/questoes/aula-1.mjs
@@ -41,7 +41,7 @@ window.AULA_DATA = {
       rotulos: [
         "SUJEITO",
         "VERBO;PREDICADO",
-        "COMPLEMENTO"
+        "COMPLEMENTO;PREDICADO"
       ],
       instrucaoDestaque: []
     }
