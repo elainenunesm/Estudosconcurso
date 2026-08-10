b46a196c4f5b9be9cf3c92b8b1e02e681ea699b8
elanunesm <elaine-nunes-m@live.com>
2026-08-10 20:30:59 -0300

    Atualização do projeto — 10/08/2026, 20:30:59

--- js/data/questoes/aula-3.mjs
+++ js/data/questoes/aula-3.mjs
@@ -59,11 +59,13 @@ window.AULA_DATA = {
   {
     tipo: "pergunta",
     texto: "Para realizar essas atividades, a lei concede alguns poderes especiais à Administração Pública.",
-    conclusao: "",
+    conclusao: "Imagine um guarda de trânsito.\n\nVocê pode aplicar uma multa? ❌ Não.\nO guarda pode? ✅ Sim.\n\nIsso acontece porque a lei concedeu esse poder ao agente público.",
     obs: "",
     pontos: [],
     textoDestaque: [],
-    textoDestaqueNegrito: []
+    textoDestaqueNegrito: [],
+    conclusaoDestaque: [],
+    conclusaoDestaqueNegrito: []
   }
 ],
 
