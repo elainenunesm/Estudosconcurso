427a2ff1f4e5465a3ec28388bebd7a6eb98cb475
elanunesm <elaine-nunes-m@live.com>
2026-08-09 22:08:24 -0300

    Atualização do projeto — 09/08/2026, 22:08:24

--- js/data/questoes/aula-1.mjs
+++ js/data/questoes/aula-1.mjs
@@ -144,7 +144,7 @@ window.AULA_DATA = {
     feedbackCorreto: "",
     feedbackErrado: "",
     opcoes: [
-      "A palavra que dá nome ",
+      "A palavra que dá nome as pessoas ",
       "",
       ""
     ],
