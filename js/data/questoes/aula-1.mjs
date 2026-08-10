687669a499a950551383c407f161a298c8cd38cb
elanunesm <elaine-nunes-m@live.com>
2026-08-09 22:08:41 -0300

    Atualização do projeto — 09/08/2026, 22:08:41

--- js/data/questoes/aula-1.mjs
+++ js/data/questoes/aula-1.mjs
@@ -144,7 +144,7 @@ window.AULA_DATA = {
     feedbackCorreto: "",
     feedbackErrado: "",
     opcoes: [
-      "A palavra que dá nome as pessoas, lugares ",
+      "A palavra que dá nome as pessoas, lugares ou coisas.",
       "",
       ""
     ],
