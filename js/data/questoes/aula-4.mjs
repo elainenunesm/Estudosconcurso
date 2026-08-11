b2ee8b91826eb657896f4b5223787ecf6f5c0a8a
elanunesm <elaine-nunes-m@live.com>
2026-08-10 21:18:27 -0300

    Atualização do projeto — 10/08/2026, 21:18:27

--- js/data/questoes/aula-4.mjs
+++ js/data/questoes/aula-4.mjs
@@ -102,7 +102,7 @@ window.AULA_DATA = {
   },
   {
     titulo: "",
-    textoAntes: "Se a administração concede poderes(prerrogativas), então, o administrador pode \nNão — a Administração não pode fazer qualquer coisa.\n\nOs poderes concedidos pela lei possuem limites, que existem para proteger o patrimônio e o interesse público. O administrador:",
+    textoAntes: "Se a administração concede poderes(prerrogativas), então, o administrador pode fazer qualquer coisa,\nNão — a Administração não pode fazer qualquer coisa.\n\nOs poderes concedidos pela lei possuem limites, que existem para proteger o patrimônio e o interesse público. O administrador:",
     itens: [],
     descricao: "",
     textoAntesDestaque: [],
